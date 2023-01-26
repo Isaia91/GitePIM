c6e6042f459ad75d4b8270d2d8b9bae56029f3eb
Grégory <greg.eres.07@hotmail.com>
2023-01-26 11:37:42 +1100

    add post method

--- script.js
+++ script.js
@@ -1,6 +1,83 @@
-const paddleDateInput = document.querySelector("#paddleDateReservation");
+function addKanoeReservation() {
+    const token = "D@lL@5Mùl!P@5S3"
+    const service = 'gite'
+    const object = 'activityreservation'
+    const action = 'create'
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 201) {
+        var x = this.responseText;
+        var xparsed = JSON.parse(x);
+      }
+    };
+    xhttp.open(
+      "POST",
+      "https://tst.quantiq.nc/devweb-cfa/api/index.php?service=gite&object=activityreservation&action=create",
+      true
+    );
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  
+    xhttp.send(
+        "token=" + token +
+        "&" + 
+        "service=" + service +
+        "&" + 
+        "object=" + object +
+        "&" + 
+        "action=" + action +
+        "&" + 
+      "date=" +
+        document.querySelector("#paddleDateReservation").value +
+        "&" +
+        "heureDebut=" +
+        document.querySelector("#paddleHeureDeDebut").value +
+        "&" +
+        "heureFin=" +
+        document.querySelector("#paddleHeureDeFin").value +
+        "&" +
+        "activite=" +
+        document.querySelector("#paddle").value
+    );
+  }
 
-paddleDateInput.addEventListener("input", event => {
-    const date = new Date(event.target.value);
-    console.log(date)
-});
+function addDinerReservation() {
+    const token = "D@lL@5Mùl!P@5S3"
+    const service = 'gite'
+    const object = 'activityreservation'
+    const action = 'create'
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 201) {
+        var x = this.responseText;
+        var xparsed = JSON.parse(x);
+      }
+    };
+    xhttp.open(
+      "POST",
+      "https://tst.quantiq.nc/devweb-cfa/api/index.php?service=gite&object=activityreservation&action=create",
+      true
+    );
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  
+    xhttp.send(
+        "token=" + token +
+        "&" + 
+        "service=" + service +
+        "&" + 
+        "object=" + object +
+        "&" + 
+        "action=" + action +
+        "&" + 
+      "date=" +
+        document.querySelector("#paddleDateReservation").value +
+        "&" +
+        "heureDebut=" +
+        document.querySelector("#paddleHeureDeDebut").value +
+        "&" +
+        "heureFin=" +
+        document.querySelector("#paddleHeureDeFin").value +
+        "&" +
+        "activite=" +
+        document.querySelector("#paddle").value
+    );
+  }
