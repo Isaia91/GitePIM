68cdcdd58bbb2d4a55f63d1de11b4fd80abd155c
AnselmTrinh <112839229+AnselmTrinh@users.noreply.github.com>
2023-01-26 13:50:58 +1100

    pull

--- script.js
+++ script.js
@@ -1,83 +1,91 @@
 function addKanoeReservation() {
-    const token = "D@lL@5Mùl!P@5S3"
-    const service = 'gite'
-    const object = 'activityreservation'
-    const action = 'create'
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 201) {
-        var x = this.responseText;
-        var xparsed = JSON.parse(x);
-      }
-    };
-    xhttp.open(
-      "POST",
-      "https://tst.quantiq.nc/devweb-cfa/api/index.php?service=gite&object=activityreservation&action=create",
-      true
-    );
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  
-    xhttp.send(
-        "token=" + token +
-        "&" + 
-        "service=" + service +
-        "&" + 
-        "object=" + object +
-        "&" + 
-        "action=" + action +
-        "&" + 
+  const token = "D@lL@5Mùl!P@5S3";
+  const service = "gite";
+  const object = "activityreservation";
+  const action = "create";
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 201) {
+      var x = this.responseText;
+      var xparsed = JSON.parse(x);
+    }
+  };
+  xhttp.open(
+    "POST",
+    "https://tst.quantiq.nc/devweb-cfa/api/index.php?service=gite&object=activityreservation&action=create",
+    true
+  );
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  xhttp.send(
+    "token=" +
+      token +
+      "&" +
+      "service=" +
+      service +
+      "&" +
+      "object=" +
+      object +
+      "&" +
+      "action=" +
+      action +
+      "&" +
       "date=" +
-        document.querySelector("#paddleDateReservation").value +
-        "&" +
-        "heureDebut=" +
-        document.querySelector("#paddleHeureDeDebut").value +
-        "&" +
-        "heureFin=" +
-        document.querySelector("#paddleHeureDeFin").value +
-        "&" +
-        "activite=" +
-        document.querySelector("#paddle").value
-    );
-  }
+      document.querySelector("#paddleDateReservation").value +
+      "&" +
+      "heureDebut=" +
+      document.querySelector("#paddleHeureDeDebut").value +
+      "&" +
+      "heureFin=" +
+      document.querySelector("#paddleHeureDeFin").value +
+      "&" +
+      "activite=" +
+      document.querySelector("#paddle").value
+  );
+}
 
 function addDinerReservation() {
-    const token = "D@lL@5Mùl!P@5S3"
-    const service = 'gite'
-    const object = 'activityreservation'
-    const action = 'create'
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 201) {
-        var x = this.responseText;
-        var xparsed = JSON.parse(x);
-      }
-    };
-    xhttp.open(
-      "POST",
-      "https://tst.quantiq.nc/devweb-cfa/api/index.php?service=gite&object=activityreservation&action=create",
-      true
-    );
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  
-    xhttp.send(
-        "token=" + token +
-        "&" + 
-        "service=" + service +
-        "&" + 
-        "object=" + object +
-        "&" + 
-        "action=" + action +
-        "&" + 
+  const token = "D@lL@5Mùl!P@5S3";
+  const service = "gite";
+  const object = "activityreservation";
+  const action = "create";
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 201) {
+      var x = this.responseText;
+      var xparsed = JSON.parse(x);
+    }
+  };
+  xhttp.open(
+    "POST",
+    "https://tst.quantiq.nc/devweb-cfa/api/index.php?service=gite&object=activityreservation&action=create",
+    true
+  );
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+  xhttp.send(
+    "token=" +
+      token +
+      "&" +
+      "service=" +
+      service +
+      "&" +
+      "object=" +
+      object +
+      "&" +
+      "action=" +
+      action +
+      "&" +
       "date=" +
-        document.querySelector("#paddleDateReservation").value +
-        "&" +
-        "heureDebut=" +
-        document.querySelector("#paddleHeureDeDebut").value +
-        "&" +
-        "heureFin=" +
-        document.querySelector("#paddleHeureDeFin").value +
-        "&" +
-        "activite=" +
-        document.querySelector("#paddle").value
-    );
-  }
+      document.querySelector("#paddleDateReservation").value +
+      "&" +
+      "heureDebut=" +
+      document.querySelector("#paddleHeureDeDebut").value +
+      "&" +
+      "heureFin=" +
+      document.querySelector("#paddleHeureDeFin").value +
+      "&" +
+      "activite=" +
+      document.querySelector("#paddle").value
+  );
+}
